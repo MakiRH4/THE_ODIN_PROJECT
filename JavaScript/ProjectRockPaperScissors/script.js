0eed9b86046efbde7cf000715d88caad7a60553a
fleonte <fleonte>
2024-10-16 21:54:41 +0200

    sync

--- JavaScript/ProjectRockPaperScissors/script.js
+++ JavaScript/ProjectRockPaperScissors/script.js
@@ -3,22 +3,45 @@ console.log("This is Rock, Paper, Scissors!!!")
 
 function	getComputerChoice() {
 	let choice = Math.random();
-	console.log(choice);
+//	console.log(choice);
 	
 	if (choice <= 0.33) {
 		console.log("Stein!");
-		return "Stein";
+		return "stein";
 	} else if (choice > 0.33 && choice <= 0.66) {
 		console.log("Papier!");
-		return "Stein";
+		return "papier";
 	} else {
 		console.log("Schere");
-		return "Stein";
+		return "schere";
 	}
 }
-getComputerChoice();
 
 function	getHumanChoice() {
+	const prompt = require('prompt-sync')();
 	return prompt("What's your choice, human?!");
 }
-getHumanChoice();
+
+function gameLogic() {
+	const humanChoice = getHumanChoice().toLowerCase();
+	const computerChoice = getComputerChoice().toLowerCase();
+
+	if (humanChoice == computerChoice) {
+		console.log("Unentschieden!");
+		return "Unentschieden!";
+	} else if (humanChoice == "papier" && computerChoice == "stein") {
+		console.log("Mensch gewinnt!");
+		return "Mensch gewinnt!";
+	} else if (humanChoice == "stein" && computerChoice == "scheren") {
+		console.log("Mensch gewinnt!");
+		return "Mensch gewinnt!";
+	} else if (humanChoice == "scheren" && computerChoice == "papier") {
+		console.log("Mensch gewinnt!");
+		return "Mensch gewinnt!";
+	} else {
+		console.log("Mensch verloren!");
+		return "Mensch verloren";
+	}
+}
+
+gameLogic();
