4dafade57088a7deeac01e9194a93d61b5724501
fleonte <fleonte>
2024-10-17 02:28:03 +0200

    sync

--- JavaScript/javascript-exercises/02_repeatString/repeatString.js
+++ JavaScript/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+	let i = 0;
+	let string = "";
+	while (i < times) {
+		string += word;
+		i++;
+	}
+	return string;
 };
 
 // Do not edit below this line
--- JavaScript/javascript-exercises/03_reverseString/reverseString.js
+++ JavaScript/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	const len = string.length;
+	let reversed = "";
+	let i = 0;
+	let j = len - 1;
 
+	while (i < len) {
+		reversed += string[j];
+		i++;
+		j--;
+	}
+//	console.log(reversed);
+	return reversed;
 };
 
 // Do not edit below this line
