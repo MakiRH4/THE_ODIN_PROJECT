7b66f1debc6050526ea0ea3731759d0df4266396
fleonte <fleonte>
2024-10-17 20:53:31 +0200

    sync

--- JavaScript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ JavaScript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+	// let argumentos = Array.from(args);
+	// let returned_array = [];
+	
+	// for (let i = 0; i < array.length; i++) {
+	// 	for (let j = 0; j < argumentos.length; j++) {
+	// 		if (array[i] == argumentos[j]) {
+	// 			delete array[i];
+	// 		}
+	// 	}
+	// }
+	
+	// for (let k = 0, l = 0; k < array.length; k++) {
+	// 	if (array[k] != undefined) {
+	// 		returned_array[l] = array[k];
+	// 		l++;
+	// 	}
+	// }
+	// return returned_array;
+	const newArray = [];
+	array.forEach((item) => {
+		if (!args.includes(item)) {
+			newArray.push(item);
+		}
+	});
+	return newArray;
+}
 
-// Do not edit below this line
 module.exports = removeFromArray;
